10c9b244799ab3877d2f0f88804f060209b0d7f3
top350 <44634314+top350@users.noreply.github.com>
2023-01-19 19:08:08 +0700

    Update card flip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import ReactCardFlip from "react-card-flip";
+import { useState } from "react";
+import SwipeReact from 'swipe-react';
+import amity from './image/amity-no-bg.png'
 function App() {
+  const [isFlip, setIsFlip] = useState(false);
+  function handleClick() {
+    setIsFlip(!isFlip);
+  }
+  const cardStyle = {
+    front: { background: "#02845f", width: "360px", height: "250px", borderRadius: '20px' , border:'1px solid #008e66', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'},
+    back: { background: "#c7f9ea", width: "360px", height: "250px",  borderRadius: '20px',border:'1px solid #008e66',boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' },
+  };
+  SwipeReact.config({
+    left: () => {
+      handleClick()
+      console.log('Swipe left detected.');
+    },
+    right: () => {
+      handleClick()
+      console.log('Swipe right detected.');
+    },
+  
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div {...SwipeReact.events}  className="card-wrapper" onClick={handleClick}>
+      <ReactCardFlip
+        cardStyles={cardStyle}
+        isFlipped={isFlip}
+        flipDirection="horizontal"
+      >
+        <div className="front-card" >
+          <div>
+          This is the front of the card.
+          </div>
+          <div>
+            name: Top Thanaphon
+          </div>
+        
+        </div>
+
+        <div className="back-card">
+        <img alt='logo' src={amity}/>
+        </div>
+      </ReactCardFlip>
+      </div>
     </div>
   );
 }
